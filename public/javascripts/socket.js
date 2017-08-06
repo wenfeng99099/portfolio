41a9fac0838c66a80890da6fdcbc895c76c2781a
Wenfeng <wenfeng99099@gmail.com>
2017-08-06 02:17:04 -0400

    can emit msg to every connected socket

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -1,2 +1,37 @@
-//establish connection
-var socket = io.connect('http://localhost:3000');
+$(document).ready(function(){
+    //establish connection
+    var socket = io.connect('http://localhost:3000');
+
+    //get DOM variables
+    var chatmsg = $('#chatMessage');
+    var chatwindow = $('#chatWindow');
+
+    $('#sendMsg').click(function(){
+        //emit from front to server
+        emitMsg();
+    });
+
+    $(document).keypress(function(e){
+       if(e.which == 13){
+           emitMsg();
+       }
+    });
+
+    //listen for chat events
+    socket.on('chat', function(data){
+        chatwindow.append("<p>" + data.msg + "</p>");
+    });
+
+    function emitMsg (){
+        socket.emit('chat', {
+            msg: chatmsg.val()
+        });
+        chatmsg.val("");
+    }
+
+});
+
+
+
+
+
